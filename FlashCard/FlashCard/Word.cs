9bb6c1b47fd8738c2ea4a8a2cb4cebf45120ba08
Tokky_tane <mitokinksa-dos3zero@outlook.jp>
2018-03-06 15:45:34 +0900

    XMLデシリアライズ用のクラスを追加

---
+++ FlashCard/FlashCard/Word.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace FlashCard2
+{
+    public class Word
+    {
+        public string Name { get; set; }
+        public string Field { get; set; }
+        public string Mean { get; set; }
+    }
+}
---
+++ FlashCard/FlashCard/WordCollection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace FlashCard2
+{
+    [XmlRoot("Words")]
+    public class WordCollection
+    {
+        [XmlElement(Type = typeof(Word), ElementName = "Word")]
+        public List<Word> Words { get; set; }
+    }
+}
